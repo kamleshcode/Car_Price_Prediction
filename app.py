fc9cc4ede2bb5c031e9aad2f1345591017257e3a
kamleshcode <kamleshptl2003@gmail.com>
2024-07-13 13:04:22 +0530

    updated

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-import subprocess
+# 
+
+
+
 import streamlit as st
 import pandas as pd
 
-from carpriceproject.components.data_evalution import ModelEvaluation
-from carpriceproject.components.data_transformation import DataTransformation
-from carpriceproject.entity.config_entity import ModelEvaluationConfig
 from carpriceproject.pipeline.prediction import PredictionPipeline
+from carpriceproject.components.data_evaluation import ModelEvaluation
+from carpriceproject.entity.config_entity import ModelEvaluationConfig
 
 # Set page configuration
 st.set_page_config(
@@ -19,7 +21,7 @@ st.set_page_config(
 st.markdown(
     """
     <style>
-    .stApp{
+    .stApp {
         background-image: url("static/background.jpg");
         background-size: cover;
         background-position: center;
@@ -46,23 +48,40 @@ st.markdown(
 st.markdown('<div class="header">🚗 Car Price Prediction 🚗</div>', unsafe_allow_html=True)
 st.markdown('<div class="subheader">Upload your car data and predict prices</div>', unsafe_allow_html=True)
 
+# Function to preprocess data for prediction
+def preprocess_data(input_data):
+    # Encode categorical variables
+    input_data['owner'].replace(['First Owner', 'Second Owner', 'Third Owner',
+                                 'Fourth & Above Owner', 'Test Drive Car'],
+                                [1, 2, 3, 4, 5], inplace=True)
+    input_data['fuel'].replace(['Diesel', 'Petrol', 'LPG', 'CNG'], [1, 2, 3, 4], inplace=True)
+    input_data['seller_type'].replace(['Individual', 'Dealer', 'Trustmark Dealer'], [1, 2, 3], inplace=True)
+    input_data['transmission'].replace(['Manual', 'Automatic'], [1, 2], inplace=True)
+    input_data['name'].replace(['Maruti', 'Skoda', 'Honda', 'Hyundai', 'Toyota', 'Ford', 'Renault',
+                                'Mahindra', 'Tata', 'Chevrolet', 'Datsun', 'Jeep', 'Mercedes-Benz',
+                                'Mitsubishi', 'Audi', 'Volkswagen', 'BMW', 'Nissan', 'Lexus',
+                                'Jaguar', 'Land', 'MG', 'Volvo', 'Daewoo', 'Kia', 'Fiat', 'Force',
+                                'Ambassador', 'Ashok', 'Isuzu', 'Opel'],
+                               [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+                               inplace=True)
+    
+    return input_data
+
 # Function to start prediction
 def start_predicting():
-    cars_data = pd.read_csv('Cardetails.csv')
-
-    def get_brand_name(car_name):
-        car_name = car_name.split(' ')[0]
-        return car_name.strip()
-
-    cars_data['name'] = cars_data['name'].apply(get_brand_name)
-
-    name = st.selectbox('🚘 Select Car Brand', cars_data['name'].unique())
+    # User input for prediction
+    name = st.selectbox('🚘 Select Car Brand', ['Maruti', 'Skoda', 'Honda', 'Hyundai', 'Toyota', 'Ford', 'Renault',
+                                               'Mahindra', 'Tata', 'Chevrolet', 'Datsun', 'Jeep', 'Mercedes-Benz',
+                                               'Mitsubishi', 'Audi', 'Volkswagen', 'BMW', 'Nissan', 'Lexus',
+                                               'Jaguar', 'Land', 'MG', 'Volvo', 'Daewoo', 'Kia', 'Fiat', 'Force',
+                                               'Ambassador', 'Ashok', 'Isuzu', 'Opel'])
     year = st.slider('📅 Car Manufactured Year', 1994, 2024)
     km_driven = st.slider('🛣️ No of kms Driven', 11, 200000)
-    fuel = st.selectbox('⛽ Fuel type', cars_data['fuel'].unique())
-    seller_type = st.selectbox('🏢 Seller type', cars_data['seller_type'].unique())
-    transmission = st.selectbox('🔄 Transmission type', cars_data['transmission'].unique())
-    owner = st.selectbox('👤 Owner type', cars_data['owner'].unique())
+    fuel = st.selectbox('⛽ Fuel type', ['Diesel', 'Petrol', 'LPG', 'CNG'])
+    seller_type = st.selectbox('🏢 Seller type', ['Individual', 'Dealer', 'Trustmark Dealer'])
+    transmission = st.selectbox('🔄 Transmission type', ['Manual', 'Automatic'])
+    owner = st.selectbox('👤 Owner type', ['First Owner', 'Second Owner', 'Third Owner',
+                                           'Fourth & Above Owner', 'Test Drive Car'])
     mileage = st.slider('⚡ Car Mileage (kmpl)', 10, 40)
     engine = st.slider('🔧 Engine CC', 700, 5000)
     max_power = st.slider('💪 Max Power (bhp)', 0, 200)
@@ -74,21 +93,11 @@ def start_predicting():
             columns=['name', 'year', 'km_driven', 'fuel', 'seller_type', 'transmission', 'owner', 'mileage', 'engine', 'max_power', 'seats']
         )
         
-        input_data_model['owner'].replace(['First Owner', 'Second Owner', 'Third Owner',
-                                           'Fourth & Above Owner', 'Test Drive Car'],
-                                          [1, 2, 3, 4, 5], inplace=True)
-        input_data_model['fuel'].replace(['Diesel', 'Petrol', 'LPG', 'CNG'], [1, 2, 3, 4], inplace=True)
-        input_data_model['seller_type'].replace(['Individual', 'Dealer', 'Trustmark Dealer'], [1, 2, 3], inplace=True)
-        input_data_model['transmission'].replace(['Manual', 'Automatic'], [1, 2], inplace=True)
-        input_data_model['name'].replace(['Maruti', 'Skoda', 'Honda', 'Hyundai', 'Toyota', 'Ford', 'Renault',
-                                          'Mahindra', 'Tata', 'Chevrolet', 'Datsun', 'Jeep', 'Mercedes-Benz',
-                                          'Mitsubishi', 'Audi', 'Volkswagen', 'BMW', 'Nissan', 'Lexus',
-                                          'Jaguar', 'Land', 'MG', 'Volvo', 'Daewoo', 'Kia', 'Fiat', 'Force',
-                                          'Ambassador', 'Ashok', 'Isuzu', 'Opel'],
-                                         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
-                                         inplace=True)
+        # Preprocess input data
+        input_data_model = preprocess_data(input_data_model)
         
-        prediction_pipeline = PredictionPipeline(model_path='artifacts/model_trainer/model.joblib')
+        # Perform prediction using PredictionPipeline
+        prediction_pipeline = PredictionPipeline()
         predictions = prediction_pipeline.predict(input_data_model)
         
         st.markdown(f'**Predicted Car Price:** ₹ {predictions[0]:.2f}')
